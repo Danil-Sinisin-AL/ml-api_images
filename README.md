a7a646b78d014f378f4da52cf2217ede6af88637
Danil-Sinisin-AL <127743230+Danil-Sinisin-AL@users.noreply.github.com>
2025-01-06 20:48:59 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ml-api_images
 Проект по распознованию животных
+
+1) Клонировать репозиторий git clon
+
+2) Создайте виртуальное окружение python
+
+python -m venv venv
+
+3) Активируйте виртуальное окружение python
+
+venv\Scripts\activate.bat - для Windows
+source venv/bin/activate - для Linux и MacOS
+
+4) Установите необходимые зависимости
+
+pip install -r requirements.txt --no-cache-dir
+
+5) Запустите приложение
+
+uvicorn main:app
+
+6) Приложение откроется по ссылке - http://localhost:8000
---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import FastAPI, File, UploadFile
+from sklearn.linear_model import LinearRegression
+from ultralytics import YOLO
+import numpy as np
+import cv2
+from typing import List, Tuple
+from PIL import Image
+
+#ort_session = ort.InferenceSession("yolo11x.onnx")
+model = YOLO("yolo11n.pt") 
+app = FastAPI()
+
+
+
+@app.get("/info")
+def info():
+    return '111'
+
+@app.post('/predict2')
+async def predict2(file: UploadFile = File(...)):
+    img = Image.open(file.file)
+    #image = cv2.cvtColor(np.frombuffer(file.read(), np.uint8), cv2.COLOR_RGB2BGR)
+    results = model(img, save_txt = True)
+    predictions = results[0]
+    results[0].show()
+    return {"111":predictions.boxes.conf.tolist()}
